cd94f2a1b695f467e14fdea54d4e9c302b9da76b
이수민 <judy3504@naver.com>
2024-05-08 15:28:15 +0900

    [chore] update chore thing

--- devway/src/components/Header.tsx
+++ devway/src/components/Header.tsx
@@ -1,12 +1,5 @@
-import styled from "styled-components";
-import logo from "../src/assets/img/devway.svg";
-
 function Header() {
-  return (
-    <div>
-      <img src={logo} alt="devway logo" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default Header;
