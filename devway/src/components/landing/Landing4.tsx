96b3b5dae2b45b42a561951bfe838aab60594f97
김동학 <kdh19583@gmail.com>
2024-05-18 18:31:55 +0900

    [feat] landing4, landing5 대충완료

--- devway/src/components/landing/Landing4.tsx
+++ devway/src/components/landing/Landing4.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import SAUCES from "@/assets/img/landing/sauces.svg";
 
 interface LandingProps {
   isActive: boolean;
@@ -7,15 +8,38 @@ interface LandingProps {
 
 const Section = styled.section`
   height: 100vh;
+  width: 100vw;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+  text-align: center;
+`;
+
+const Text = styled.div`
+  margin-bottom: 20px;
+  font-size: 24px;
+  font-weight: bold;
+  color: white;
+`;
+
+const SubText = styled.div`
+  margin-bottom: 40px;
+  font-size: 18px;
+  color: #555;
+`;
+
+const SauceImage = styled.img`
+  width: auto;
+  height: auto;
 `;
 
 const Landing4: React.FC<LandingProps> = ({ isActive }) => {
   return (
     <Section>
-      <h1>Landing Page 4</h1>
+      <Text>원하는 소스가 없을 땐?</Text>
+      <SubText>if there is no sauce which you want</SubText>
+      <SauceImage src={SAUCES} alt="Sauces" />
     </Section>
   );
 };
