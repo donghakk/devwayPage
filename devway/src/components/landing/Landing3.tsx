aaaf66c12417ac0d36c69c3bd32118ea1dfa7ba9
김동학 <kdh19583@gmail.com>
2024-05-18 18:39:17 +0900

    [fix] landing3 플로우 속도 느리게

--- devway/src/components/landing/Landing3.tsx
+++ devway/src/components/landing/Landing3.tsx
@@ -31,6 +31,78 @@ const profiles = [
     review:
       "초보자도 쉽게 따라 할 수 있는 가이드가 많아 정말 도움이 많이 됐습니다. 감사합니다!",
   },
+  {
+    image: "https://via.placeholder.com/150",
+    name: "28세 프론트엔드 개발자",
+    review:
+      "오픈 API가 가끔 사용하기 어렵게 되어있는데, 데브웨이를 통해서 편하게 쓸 수 있었어요! 데브웨이 감사합니다.",
+  },
+  {
+    image: "https://via.placeholder.com/150",
+    name: "35세 백엔드 개발자",
+    review:
+      "데브웨이를 통해 복잡한 백엔드 설정을 쉽게 할 수 있었습니다. 강력 추천합니다!",
+  },
+  {
+    image: "https://via.placeholder.com/150",
+    name: "30세 풀스택 개발자",
+    review:
+      "풀스택 개발자로서 데브웨이는 정말 유용한 도구입니다. 개발 생산성이 크게 향상되었습니다.",
+  },
+  {
+    image: "https://via.placeholder.com/150",
+    name: "25세 주니어 개발자",
+    review:
+      "초보자도 쉽게 따라 할 수 있는 가이드가 많아 정말 도움이 많이 됐습니다. 감사합니다!",
+  },
+  {
+    image: "https://via.placeholder.com/150",
+    name: "28세 프론트엔드 개발자",
+    review:
+      "오픈 API가 가끔 사용하기 어렵게 되어있는데, 데브웨이를 통해서 편하게 쓸 수 있었어요! 데브웨이 감사합니다.",
+  },
+  {
+    image: "https://via.placeholder.com/150",
+    name: "35세 백엔드 개발자",
+    review:
+      "데브웨이를 통해 복잡한 백엔드 설정을 쉽게 할 수 있었습니다. 강력 추천합니다!",
+  },
+  {
+    image: "https://via.placeholder.com/150",
+    name: "30세 풀스택 개발자",
+    review:
+      "풀스택 개발자로서 데브웨이는 정말 유용한 도구입니다. 개발 생산성이 크게 향상되었습니다.",
+  },
+  {
+    image: "https://via.placeholder.com/150",
+    name: "25세 주니어 개발자",
+    review:
+      "초보자도 쉽게 따라 할 수 있는 가이드가 많아 정말 도움이 많이 됐습니다. 감사합니다!",
+  },
+  {
+    image: "https://via.placeholder.com/150",
+    name: "28세 프론트엔드 개발자",
+    review:
+      "오픈 API가 가끔 사용하기 어렵게 되어있는데, 데브웨이를 통해서 편하게 쓸 수 있었어요! 데브웨이 감사합니다.",
+  },
+  {
+    image: "https://via.placeholder.com/150",
+    name: "35세 백엔드 개발자",
+    review:
+      "데브웨이를 통해 복잡한 백엔드 설정을 쉽게 할 수 있었습니다. 강력 추천합니다!",
+  },
+  {
+    image: "https://via.placeholder.com/150",
+    name: "30세 풀스택 개발자",
+    review:
+      "풀스택 개발자로서 데브웨이는 정말 유용한 도구입니다. 개발 생산성이 크게 향상되었습니다.",
+  },
+  {
+    image: "https://via.placeholder.com/150",
+    name: "25세 주니어 개발자",
+    review:
+      "초보자도 쉽게 따라 할 수 있는 가이드가 많아 정말 도움이 많이 됐습니다. 감사합니다!",
+  },
   // 더 많은 더미 데이터 추가 가능
 ];
 
@@ -132,7 +204,7 @@ const BannerContainer = styled.div`
 
 const Banner = styled.div`
   display: flex;
-  animation: ${scrollAnimation} 10s linear infinite;
+  animation: ${scrollAnimation} 100s linear infinite;
   > div {
     flex-shrink: 0;
     margin-right: 50px;
