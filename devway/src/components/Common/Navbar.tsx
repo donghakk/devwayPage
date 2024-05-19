1542340094840d3878ed28b81c9d995bd7f8c0d1
김동학 <kdh19583@gmail.com>
2024-05-19 18:16:09 +0900

    [feat] 가이드 다운로드 추가

--- devway/src/components/Common/Navbar.tsx
+++ devway/src/components/Common/Navbar.tsx
@@ -1,80 +1,117 @@
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
-import NavbarItem from "./NavbarItem";
 import { useState } from "react";
 
 interface NavbarProps {
-    name: string;
+  name: string;
 }
 
 function Sidebar({ name }: NavbarProps) {
-    const menus = [
-        { name: "HOME", path: "/" },
-        { name: "DEVWAY", path: "/devway" },
-        { name: "BY DEVWAY", path: "/bydevway" },
-        { name: "FAQ", path: "/faq" },
-        { name: "GUIDE", path: "" },
-    ];
+  const menus = [
+    { name: "HOME", path: "/" },
+    { name: "DEVWAY", path: "/devway" },
+    { name: "BY DEVWAY", path: "/bydevway" },
+    { name: "FAQ", path: "/faq" },
+    { name: "GUIDE", path: "/guideline.html", download: true },
+  ];
 
-    const [hoveredMenu, setHoveredMenu] = useState<string | null>(null);
+  const [hoveredMenu, setHoveredMenu] = useState<string | null>(null);
 
-    return (
-        <Side>
-            {menus.map((menu, index) => (
-                <NavLinkStyled
-                    className="active"
-                    to={menu.path}
-                    key={index}
-                    isActive={menu.name === name}
-                    onMouseEnter={() => setHoveredMenu(menu.name)}
-                    onMouseLeave={() => setHoveredMenu(null)}
-                    hoveredMenu={hoveredMenu}
-                >
-                    {menu.name === "GUIDE" && hoveredMenu === "GUIDE"
-                        ? "Click to Download"
-                        : menu.name}
-                </NavLinkStyled>
-            ))}
-        </Side>
-    );
+  return (
+    <Side>
+      {menus.map((menu, index) =>
+        menu.download ? (
+          <AnchorStyled
+            href={menu.path}
+            key={index}
+            onMouseEnter={() => setHoveredMenu(menu.name)}
+            onMouseLeave={() => setHoveredMenu(null)}
+            hoveredMenu={hoveredMenu}
+            download
+          >
+            {menu.name === "GUIDE" && hoveredMenu === "GUIDE"
+              ? "Click to Download"
+              : menu.name}
+          </AnchorStyled>
+        ) : (
+          <NavLinkStyled
+            className="active"
+            to={menu.path}
+            key={index}
+            isActive={menu.name === name}
+            onMouseEnter={() => setHoveredMenu(menu.name)}
+            onMouseLeave={() => setHoveredMenu(null)}
+            hoveredMenu={hoveredMenu}
+          >
+            {menu.name}
+          </NavLinkStyled>
+        )
+      )}
+    </Side>
+  );
 }
 
 export default Sidebar;
 
 const Side = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 `;
 
 const NavLinkStyled = styled(NavLink)<{
-    isActive: boolean;
-    hoveredMenu: string | null;
+  isActive: boolean;
+  hoveredMenu: string | null;
 }>`
-    text-decoration: none;
-    background-color: white;
-    padding: 10px 10px;
-    width: 160px;
-    color: grey;
-    text-align: center;
-    &:hover {
-        color: black;
-        font-weight: bold;
-        opacity: 1;
-    }
+  text-decoration: none;
+  background-color: white;
+  padding: 10px 10px;
+  width: 160px;
+  color: grey;
+  text-align: center;
+  &:hover {
+    color: black;
+    font-weight: bold;
+    opacity: 1;
+  }
 
-    ${({ isActive }) =>
-        isActive &&
-        `
+  ${({ isActive }) =>
+    isActive &&
+    `
     color: black;
       font-weight: bold;
     `}
 
-    ${({ hoveredMenu }) =>
-        hoveredMenu === "GUIDE" &&
-        `
+  ${({ hoveredMenu }) =>
+    hoveredMenu === "GUIDE" &&
+    `
     &:hover {
         content: "Click to Download";
+        color: red;
+        transition: color 0.4s;
+    }
+`}
+`;
+
+const AnchorStyled = styled.a<{
+  hoveredMenu: string | null;
+}>`
+  text-decoration: none;
+  background-color: white;
+  padding: 10px 10px;
+  width: 160px;
+  color: grey;
+  text-align: center;
+  &:hover {
+    color: black;
+    font-weight: bold;
+    opacity: 1;
+  }
+
+  ${({ hoveredMenu }) =>
+    hoveredMenu === "GUIDE" &&
+    `
+    &:hover {
         color: red;
         transition: color 0.4s;
     }
